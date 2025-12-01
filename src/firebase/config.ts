0837b47b59544683ce18ff22fa51c1ab866db343
Crazy Jutt <crazyjutt659@gmail.com>
2025-12-01 11:31:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8561278593-6d51c",
+  "appId": "1:915877937826:web:d1f0ab2f3ef310b7760dfb",
+  "apiKey": "AIzaSyB7CIZ1h_ngeUDtOeDwBhvruwjGWW1czwM",
+  "authDomain": "studio-8561278593-6d51c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "915877937826"
+};
